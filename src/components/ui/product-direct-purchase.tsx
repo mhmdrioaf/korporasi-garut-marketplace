1a5cbc3220389b9af275953e89cd45da63087972
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2023-10-16 13:23:19 +0700

    `components` product direct-purchase modals

--- src/components/ui/product-direct-purchase.tsx
+++ src/components/ui/product-direct-purchase.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { TAddress, TProduct } from "@/lib/globals";
+import { TAddress, TProduct, TProductVariantItem } from "@/lib/globals";
 import { fetcher } from "@/lib/helper";
 import { useState } from "react";
 import useSWR from "swr";
@@ -9,12 +9,16 @@ import { useRouter } from "next/navigation";
 import { ROUTES } from "@/lib/constants";
 import DirectPurchaseAddressOptions from "./direct-purchase-address-options";
 import Modal from "./modal";
+import DirectPurchaseOrderDetail from "./direct-purchase-order-detail";
+import { CheckCircle2Icon } from "lucide-react";
+import Link from "next/link";
 
 interface IProductDirectPurchaseComponentProps {
   product: TProduct;
   user_id: string | null;
   product_quantity: number;
-  product_variant: string | null;
+  product_variant: TProductVariantItem | null;
+  totalPrice: number;
 }
 
 export default function ProductDirectPurchase({
@@ -22,9 +26,9 @@ export default function ProductDirectPurchase({
   product_quantity,
   product_variant,
   user_id,
+  totalPrice,
 }: IProductDirectPurchaseComponentProps) {
   const [orderStep, setOrderStep] = useState<number | null>(null);
-  const [shouldLogin, setShouldLogin] = useState(false);
   const [chosenAddress, setChosenAddress] = useState<TAddress | null>(null);
 
   const router = useRouter();
@@ -55,7 +59,7 @@ export default function ProductDirectPurchase({
           defaultOpen={orderStep !== null}
           onClose={() => setOrderStep(null)}
         >
-          <div className="w-full flex flex-col gap-4">
+          <div className="w-full max-h-[85vh] overflow-y-auto flex flex-col gap-4">
             <div className="w-full flex flex-row items-center gap-2 justify-center">
               {[...Array(3)].map((_, index) => (
                 <div className="flex flex-row items-center gap-2" key={index}>
@@ -80,6 +84,32 @@ export default function ProductDirectPurchase({
               user={user?.result}
               user_address={user?.result?.address}
             />
+
+            <DirectPurchaseOrderDetail
+              product={product}
+              product_quantity={product_quantity}
+              product_variant={product_variant}
+              shipping_address={chosenAddress!}
+              user_id={user_id}
+              totalPrice={totalPrice}
+              isOpen={isModalOpen(2)}
+              setOrderStep={setOrderStep}
+            />
+
+            {isModalOpen(3) && (
+              <div className="w-full flex flex-col gap-4">
+                <div className="w-full flex flex-col gap-2 items-center justify-center">
+                  <CheckCircle2Icon className="w-10 h-10 text-green-950" />
+                  <p className="text-2xl font-bold">Pemesanan Berhasil!</p>
+                </div>
+                <Link
+                  href={ROUTES.USER.ORDERS}
+                  className="px-4 py-2 rounded-sm text-sm bg-primary text-primary-foreground text-center"
+                >
+                  Lihat Pesanan
+                </Link>
+              </div>
+            )}
           </div>
         </Modal>
       )}
