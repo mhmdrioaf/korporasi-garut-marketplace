398ea5f5e4ad0c826ef04cfddd4bd25a46341b1c
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2023-10-23 08:34:58 +0700

    `image` unoptimized

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    deviceSizes: [640, 750, 828, 1080, 1200, 1366],
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
