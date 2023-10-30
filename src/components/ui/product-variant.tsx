03d61c33f1d8852d9de2180480aabc2fbe16f699
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2023-10-31 03:35:28 +0700

    implementing the new product variant model

--- src/components/ui/product-variant.tsx
+++ src/components/ui/product-variant.tsx
@@ -5,37 +5,32 @@ import { useDirectPurchase } from "@/lib/hooks/context/useDirectPurchase";
 export default function ProductVariants() {
   const { variants, product } = useDirectPurchase();
 
-  if (product.variant && product.variant.length > 0) {
+  if (product.variant) {
     return (
       <div className="w-full flex flex-col gap-4">
         <p className="text-sm font-bold">
           Harap memilih variant, jika tidak memilih, akan kami kirim secara
           acak.
         </p>
-        {product.variant.map((variant) => (
-          <div
-            key={variant.variant_id}
-            className="w-full grid grid-cols-2 gap-2"
-          >
-            <p>{variant.variant_title}</p>
-            <div className="w-full grid grid-cols-3 gap-2">
-              {variant.variant_item.map((item) => (
-                <div
-                  key={item.variant_item_id}
-                  className={`p-1 w-full rounded-sm border border-input cursor-pointer text-center select-none ${
-                    variants.variantValue?.variant_item_id ===
-                    item.variant_item_id
-                      ? "bg-green-950 text-white font-bold"
-                      : ""
-                  }`}
-                  onClick={() => variants.handler.onVariantsChange(item)}
-                >
-                  <p>{item.variant_name}</p>
-                </div>
-              ))}
-            </div>
+        <div className="w-full grid grid-cols-2 gap-2">
+          <p>{product.variant.variant_title}</p>
+          <div className="w-full grid grid-cols-3 gap-2">
+            {product.variant.variant_item.map((item) => (
+              <div
+                key={item.variant_item_id}
+                className={`p-1 w-full rounded-sm border border-input cursor-pointer text-center select-none ${
+                  variants.variantValue?.variant_item_id ===
+                  item.variant_item_id
+                    ? "bg-green-950 text-white font-bold"
+                    : ""
+                }`}
+                onClick={() => variants.handler.onVariantsChange(item)}
+              >
+                <p>{item.variant_name}</p>
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
     );
   } else {
