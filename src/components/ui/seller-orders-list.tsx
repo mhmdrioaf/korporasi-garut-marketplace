872d58630b30699be4d83ad8177d316816ff224a
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2023-11-05 05:17:56 +0700

    removing unused imports & functions

--- src/components/ui/seller-orders-list.tsx
+++ src/components/ui/seller-orders-list.tsx
@@ -1,38 +1,12 @@
 "use client";
 
-import { ORDER_STATUS } from "@/lib/globals";
-import {
-  getDateString,
-  orderStatusConverter,
-  phoneNumberGenerator,
-  rupiahConverter,
-} from "@/lib/helper";
-import Image from "next/image";
-import Link from "next/link";
-import { ROUTES } from "@/lib/constants";
-import { Separator } from "./separator";
-import { Button } from "./button";
 import OrderDeliveryReceipt from "./order-delivery-receipt";
 import { useOrderManagement } from "@/lib/hooks/context/useOrderManagement";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./tabs";
 import SellerOrderCard from "./seller-order-card";
 
 export default function SellerOrderList() {
-  const { orders, renderer } = useOrderManagement();
-
-  const orderShownButtonStyle = (isActive: boolean, isLoading: boolean) => {
-    const defaultStyle =
-      "px-4 py-2 rounded-md text-center hover:bg-primary hover:text-primary-foreground transition-all cursor-pointer select-none";
-    const activeStyle = " bg-primary text-primary-foreground";
-    const loadingStyle =
-      "w-fit px-4 py-2 rounded-md bg-input animate-pulse text-transparent";
-
-    return isActive && !isLoading
-      ? defaultStyle + activeStyle
-      : !isActive && !isLoading
-      ? defaultStyle
-      : loadingStyle;
-  };
+  const { orders } = useOrderManagement();
 
   return (
     <Tabs
