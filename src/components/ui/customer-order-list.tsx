307298a9610e6c5ba76e6f4bc0fb9b72946bae8a
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2023-11-05 06:39:35 +0700

    order delivered implementations

--- src/components/ui/customer-order-list.tsx
+++ src/components/ui/customer-order-list.tsx
@@ -1,20 +1,73 @@
 "use client";
 
-import { TCustomerOrder } from "@/lib/globals";
+import { ORDER_STATUS, TCustomerOrder } from "@/lib/globals";
 import OrderCard from "./order-card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "./tabs";
 
 interface ICustomerOrdersListProps {
   orders: TCustomerOrder[];
 }
 
+type TOrderShown = "ALL" | ORDER_STATUS;
+
 export default function CustomerOrderList({
   orders,
 }: ICustomerOrdersListProps) {
+  const ordersData = {
+    ALL: orders,
+    PENDING: orders.filter((order) => order.order_status === "PENDING"),
+    PAID: orders.filter((order) => order.order_status === "PAID"),
+    PACKED: orders.filter((order) => order.order_status === "PACKED"),
+    SHIPPED: orders.filter((order) => order.order_status === "SHIPPED"),
+    DELIVERED: orders.filter((order) => order.order_status === "DELIVERED"),
+  };
+  const orderShownLabel = (options: TOrderShown) => {
+    switch (options) {
+      case "ALL":
+        return "Semua";
+      case "PAID":
+        return "Menunggu Pengiriman";
+      case "PACKED":
+        return "Dikemas";
+      case "SHIPPED":
+        return "Sedang Dikirim";
+      case "PENDING":
+        return "Menunggu Pembayaran";
+      default:
+        return "Selesai";
+    }
+  };
   return (
-    <div className="w-full flex flex-col gap-4 lg:gap-4">
-      {orders.map((order) => (
-        <OrderCard order={order} key={order.order_id} />
-      ))}
-    </div>
+    <Tabs defaultValue="ALL" className="w-full flex flex-col gap-4 lg:gap-8">
+      <TabsList>
+        <TabsTrigger value="ALL">{orderShownLabel("ALL")}</TabsTrigger>
+        <TabsTrigger value="PENDING">{orderShownLabel("PENDING")}</TabsTrigger>
+        <TabsTrigger value="PAID">{orderShownLabel("PAID")}</TabsTrigger>
+        <TabsTrigger value="PACKED">{orderShownLabel("PACKED")}</TabsTrigger>
+        <TabsTrigger value="SHIPPED">{orderShownLabel("SHIPPED")}</TabsTrigger>
+        <TabsTrigger value="DELIVERED">
+          {orderShownLabel("DELIVERED")}
+        </TabsTrigger>
+      </TabsList>
+
+      <TabsContent value="ALL">
+        <OrderCard ordersData={ordersData.ALL} />
+      </TabsContent>
+      <TabsContent value="PENDING">
+        <OrderCard ordersData={ordersData.PENDING} />
+      </TabsContent>
+      <TabsContent value="PAID">
+        <OrderCard ordersData={ordersData.PAID} />
+      </TabsContent>
+      <TabsContent value="PACKED">
+        <OrderCard ordersData={ordersData.PACKED} />
+      </TabsContent>
+      <TabsContent value="SHIPPED">
+        <OrderCard ordersData={ordersData.SHIPPED} />
+      </TabsContent>
+      <TabsContent value="DELIVERED">
+        <OrderCard ordersData={ordersData.DELIVERED} />
+      </TabsContent>
+    </Tabs>
   );
 }
