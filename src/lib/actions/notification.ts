bcd87eea6c67c4111b0b437d6846723e3029618c
Muhammad Rio Ananta Firdaus <77482212+mhmdrioaf@users.noreply.github.com>
2023-11-13 19:40:30 +0000

    Refactor notification actions to separate read and delete handlers

--- src/lib/actions/notification.ts
+++ src/lib/actions/notification.ts
@@ -1,6 +1,6 @@
 import { TNotification } from "../globals";
 
-export async function readNotification(
+export async function readNotificationHandler(
     body: {
         notification_id: string, 
         notification_item_id: string
@@ -8,6 +8,32 @@ export async function readNotification(
 ) {
     const res = await fetch(process.env.NEXT_PUBLIC_API_NOTIFICATION_READ!, {
         method: "PATCH",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            notification_id: body.notification_id,
+            notification_item_id: body.notification_item_id
+        })
+    })
+
+    const response = await res.json();
+
+    if (response.ok) {
+        return response.result as TNotification
+    } else {
+        return undefined
+    }
+}
+
+export async function deleteNotificationHandler(
+    body: {
+        notification_id: string, 
+        notification_item_id: string
+    }
+) {
+    const res = await fetch(process.env.NEXT_PUBLIC_API_NOTIFICATION_DELETE!, {
+        method: "DELETE",
         headers: {
             "Content-Type": "application/json"
         },
