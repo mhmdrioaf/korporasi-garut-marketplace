0a0526f38f07463c6380c2141b9508f9b2eb2856
Muhammad Rio Ananta Firdaus <77482212+mhmdrioaf@users.noreply.github.com>
2023-11-15 09:37:40 +0000

    Add order by date descending to seller orders

--- src/app/api/order/list-seller-orders/route.ts
+++ src/app/api/order/list-seller-orders/route.ts
@@ -56,6 +56,9 @@ async function handler(request: NextRequest) {
             },
           },
         },
+        orderBy: {
+          order_date: "desc",
+        },
       });
 
       if (sellerOrders) {
