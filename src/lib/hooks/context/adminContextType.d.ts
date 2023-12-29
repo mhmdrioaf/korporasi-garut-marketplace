d41e6aa14f6889dd502560ac5ceaa6bad105723c
Muhammad Rio Ananta Firdaus <77482212+mhmdrioaf@users.noreply.github.com>
2023-12-29 00:13:02 +0000

    Refactor adminContextType to include sales report data and handlers

--- src/lib/hooks/context/adminContextType.d.ts
+++ src/lib/hooks/context/adminContextType.d.ts
@@ -1,3 +1,23 @@
 type TAdminContextType = {
-  test: string;
+  credentials: {
+    token: string;
+  };
+
+  reports: {
+    sales: {
+      data: TSalesReportData[] | null | undefined;
+      startDate: string | null;
+      endDate: string | null;
+
+      state: {
+        loading: boolean;
+        error: any;
+      };
+
+      handler: {
+        changeStartDate: React.Dispatch<React.SetStateAction<string | null>>;
+        changeEndDate: React.Dispatch<React.SetStateAction<string | null>>;
+      };
+    };
+  };
 };
