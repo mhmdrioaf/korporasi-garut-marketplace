1f396922b661f4b30a9da16ef7ea2f6626af29d0
Muhammad Rio Ananta Firdaus <77482212+mhmdrioaf@users.noreply.github.com>
2023-12-29 00:13:08 +0000

    Refactor AdminProvider and add useAdmin hook

--- src/lib/hooks/context/useAdmin.tsx
+++ src/lib/hooks/context/useAdmin.tsx
@@ -1,21 +1,78 @@
 "use client";
 
-import NoAccess from "@/components/ui/no-access";
-import { ReactNode, createContext } from "react";
+import {
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import useSWR from "swr";
 
 interface IAdminProviderProps {
-  isAllowed: boolean;
+  token: string;
   children: ReactNode;
 }
 
 export const AdminContext = createContext<TAdminContextType | null>(null);
 
-export function AdminProvider({ isAllowed, children }: IAdminProviderProps) {
-  return isAllowed ? (
-    <AdminContext.Provider value={{ test: "" }}>
-      {children}
-    </AdminContext.Provider>
-  ) : (
-    <NoAccess />
+export function useAdmin() {
+  return useContext(AdminContext) as TAdminContextType;
+}
+
+export function AdminProvider({ token, children }: IAdminProviderProps) {
+  const [startDate, setStartDate] = useState<string | null>(null);
+  const [endDate, setEndDate] = useState<string | null>(null);
+
+  const fetcher = (url: string) =>
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        token: token,
+        startDate: startDate ? startDate : "2023-01-01",
+        endDate: endDate ? endDate : "2023-12-31",
+      }),
+    })
+      .then((res) => res.json())
+      .then((response) => response.data as TSalesReportData[] | null);
+
+  const {
+    data: salesData,
+    isLoading: salesReportLoading,
+    error: salesReportError,
+    mutate,
+  } = useSWR("/api/report/getSales", fetcher);
+
+  useEffect(() => {
+    if (startDate && endDate) {
+      mutate();
+    }
+  }, [startDate, endDate, mutate]);
+
+  const value: TAdminContextType = {
+    credentials: {
+      token: token,
+    },
+    reports: {
+      sales: {
+        data: salesData,
+        startDate: startDate,
+        endDate: endDate,
+
+        state: {
+          loading: salesReportLoading,
+          error: salesReportError,
+        },
+
+        handler: {
+          changeStartDate: setStartDate,
+          changeEndDate: setEndDate,
+        },
+      },
+    },
+  };
+
+  return (
+    <AdminContext.Provider value={value}>{children}</AdminContext.Provider>
   );
 }
