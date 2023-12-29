b1b926aea4ad1cec9598af57d02b3be889887c7f
Muhammad Rio Ananta Firdaus <77482212+mhmdrioaf@users.noreply.github.com>
2023-12-29 00:12:40 +0000

    Add sales report types

---
+++ src/lib/types/sales-report.d.ts
@@ -0,0 +1,47 @@
+type TSalesReportAccount = {
+  user_name: string;
+};
+
+type TSalesReportUser = {
+  user_id: number;
+  account: TSalesReportAccount | null;
+};
+
+type TSalesReportSeller = TSalesReportUser;
+
+type TSalesReportProduct = {
+  id: number;
+  images: string[];
+  price: number;
+  title: string;
+  seller: TSalesReportSeller;
+};
+
+type TSalesReportVariant = {
+  variant_item_id: string;
+  variant_name: string;
+  variant_price: number;
+};
+
+type TSalesReportOrderItem = {
+  order_quantity: number;
+  product: TSalesReportProduct;
+  variant: TSalesReportVariant | null;
+};
+
+type TSalesReportData = {
+  order_id: string;
+  order_status: string;
+  order_date: Date;
+  order_delivered_date: Date | null;
+  user_id: number;
+  total_price: number;
+  shipping_address: string | null;
+  shipping_cost: number;
+  delivery_receipt: string | null;
+  payment_proof: string | null;
+  order_type: ORDER_TYPE;
+
+  user: TSalesReportUser;
+  order_item: TSalesReportOrderItem[];
+};
