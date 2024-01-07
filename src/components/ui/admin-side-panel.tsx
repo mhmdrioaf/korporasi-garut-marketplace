081c572c88265abd0a2eba02d1a9fd200137c772
Muhammad Rio Ananta Firdaus <77482212+mhmdrioaf@users.noreply.github.com>
2024-01-07 15:56:29 +0000

    separating side panel for the admin page layout

---
+++ src/components/ui/admin-side-panel.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { ROUTES } from "@/lib/constants";
+import {
+  BookTextIcon,
+  LayoutDashboardIcon,
+  Package2Icon,
+  UserPlus2Icon,
+  Users2Icon,
+} from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
+
+export default function AdminSidePanel() {
+  const [labelShown, setLabelShown] = useState<boolean>(false);
+
+  const labelStyle = `font-bold text-sm text-center ${
+    !labelShown ? "lg:hidden" : ""
+  }`;
+
+  function handleShownLabel(shown: boolean) {
+    setLabelShown(shown);
+  }
+
+  return (
+    <div
+      onMouseOver={() => handleShownLabel(true)}
+      onMouseLeave={() => handleShownLabel(false)}
+      className="w-full bg-background z-40 lg:w-fit h-fit lg:h-screen fixed bottom-0 lg:top-[5.25rem] left-0 border-t border-r-0 border-t-input lg:border-t-0 lg:border-r lg:border-r-input flex flex-row lg:flex-col gap-2 divide-x divide-y-0 lg:divide-y lg:divide-x-0 justify-stretch lg:justify-normal"
+    >
+      <Link
+        className="w-full grid place-items-center p-2"
+        href={ROUTES.ADMIN.DASHBOARD}
+        title="Dashboard"
+      >
+        <LayoutDashboardIcon className="w-6 h-6" />
+        <p className={labelStyle}>Dashboard</p>
+      </Link>
+
+      <Link
+        className="w-full grid place-items-center p-2"
+        href={ROUTES.ADMIN.USER_MANAGEMENT.MAIN}
+        title="Kelola Pengguna"
+      >
+        <Users2Icon className="w-6 h-6" />
+        <p className={labelStyle}>Kelola Pengguna</p>
+      </Link>
+
+      <Link
+        className="w-full grid place-items-center p-2"
+        href={ROUTES.ADMIN.USER_MANAGEMENT.ADD_SELLER}
+        title="Tambahkan Penjual"
+      >
+        <UserPlus2Icon className="w-6 h-6" />
+        <p className={labelStyle}>Tambahkan Penjual</p>
+      </Link>
+
+      <Link
+        className="w-full grid place-items-center p-2"
+        href={ROUTES.ADMIN.PRODUCT_MANAGEMENT.MAIN}
+        title="Kelola Produk"
+      >
+        <Package2Icon className="w-6 h-6" />
+        <p className={labelStyle}>Kelola Produk</p>
+      </Link>
+
+      <Link
+        className="w-full grid place-items-center p-2"
+        href={ROUTES.ADMIN.REPORTS}
+        title="Laporan Penjualan"
+      >
+        <BookTextIcon className="w-6 h-6" />
+        <p className={labelStyle}>Laporan Penjualan</p>
+      </Link>
+    </div>
+  );
+}
