a8f8064aa57cc231316aceb84abbe86b9a72a026
Muhammad Rio Ananta Firdaus <77482212+mhmdrioaf@users.noreply.github.com>
2024-01-07 03:41:26 +0000

    Refactor admin reports UI components

--- src/components/ui/admin-reports.tsx
+++ src/components/ui/admin-reports.tsx
@@ -1,10 +1,19 @@
 "use client";
 
-import { Chart as ChartJS, registerables } from "chart.js";
-import { Chart } from "react-chartjs-2";
 import { useAdmin } from "@/lib/hooks/context/useAdmin";
-import { getMonthString, getSales, getTotalProducts } from "@/lib/helper";
+import { Loader2Icon } from "lucide-react";
+import { Chart as ChartJS, registerables } from "chart.js";
+import { Separator } from "./separator";
+import ReportsDatePicker from "./reports-date-picker";
+import ReportsChart from "./reports-chart";
+import {
+  getMonthString,
+  getTotalIncome,
+  getTotalProducts,
+  rupiahConverter,
+} from "@/lib/helper";
 import { useEffect } from "react";
+import ReportsProducts from "./reports-products";
 
 export default function AdminReportsComponents() {
   const { reports } = useAdmin();
@@ -22,45 +31,48 @@ export default function AdminReportsComponents() {
 
   return (
     <div className="w-full flex flex-col gap-4">
-      {reports.sales.data && (
-        <Chart
-          type="line"
-          data={{
-            labels: getMonthString(startDate, endDate),
-            datasets: [
-              {
-                data: getSales(reports.sales.data, startDate, endDate),
-                borderWidth: 1,
-                label: "Penjualan",
-              },
-            ],
-          }}
-          options={{
-            scales: {
-              y: {
-                beginAtZero: true,
-                min: 0,
-                ticks: {
-                  precision: 0,
-                },
-              },
-            },
-          }}
-        />
+      {reports.sales.state.loading ? (
+        <div className="w-full h-96 flex flex-col items-center justify-center gap-2">
+          <Loader2Icon className="text-primary w-16 h-16 animate-spin" />
+          <p className="text-sm">Memuat data...</p>
+        </div>
+      ) : (
+        <ReportsChart />
       )}
 
-      <div className="w-full flex flex-col gap-2">
-        <div className="w-full flex flex-row items-center justify-between">
-          <p className="font-bold">
-            Total Penjualan: {reports.sales.data?.length ?? 0}
-          </p>
-          {reports.sales.data && (
+      {reports.sales.data && (
+        <div className="w-full flex flex-col gap-2">
+          <div className="w-full flex flex-row items-center justify-between">
+            <p className="font-bold">
+              Total Penjualan: {reports.sales.data.length}
+            </p>
+
             <p className="font-bold">
               Total produk terjual:{" "}
               {getTotalProducts(reports.sales.data, startDate, endDate)} produk
             </p>
-          )}
+          </div>
         </div>
+      )}
+
+      <Separator />
+
+      <ReportsDatePicker />
+
+      <Separator />
+      <ReportsProducts />
+
+      <Separator />
+      <div className="w-full grid grid-cols-2">
+        <p className="font-bold">
+          Total pendapatan {getMonthString(startDate, startDate + 1)} -{" "}
+          {getMonthString(endDate, endDate + 1)} tahun {reports.sales.year}
+        </p>
+        <p className="font-bold self-center justify-self-end">
+          {reports.sales.data
+            ? rupiahConverter(getTotalIncome(reports.sales.data))
+            : rupiahConverter(0)}
+        </p>
       </div>
     </div>
   );
