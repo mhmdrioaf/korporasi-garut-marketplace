57994098ce2c1f4f1bbb28295cb827942a438937
Muhammad Rio Ananta Firdaus <77482212+mhmdrioaf@users.noreply.github.com>
2024-01-23 18:41:37 +0000

    feat(address): adding district input field

--- src/components/ui/address-form.tsx
+++ src/components/ui/address-form.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { TAddress, TCity, TProvince } from "@/lib/globals";
+import { TAddress, TCity, TDistrict, TProvince } from "@/lib/globals";
 import { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import useSWR, { useSWRConfig } from "swr";
@@ -18,8 +18,9 @@ import { useToast } from "./use-toast";
 import { useRouter } from "next/navigation";
 import { ROUTES } from "@/lib/constants";
 import { Button } from "./button";
-import { Loader2Icon } from "lucide-react";
+import { HelpCircleIcon, Loader2Icon } from "lucide-react";
 import { properizeWords } from "@/lib/helper";
+import AddressHelpModal from "./modals/address-help-modal";
 
 interface IAddressFormProps {
   userId: string;
@@ -33,6 +34,8 @@ type TAddressInput = {
   recipient_phone: string;
   city: TCity;
   label: string;
+  district: string;
+  village: string;
 };
 
 export default function AddressForm({
@@ -41,6 +44,13 @@ export default function AddressForm({
   onUpdated,
 }: IAddressFormProps) {
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [help, setHelp] = useState<{
+    option: "district" | "village";
+    open: boolean;
+  }>({
+    option: "district",
+    open: false,
+  });
 
   const form = useForm<TAddressInput>({
     defaultValues: {
@@ -52,6 +62,8 @@ export default function AddressForm({
         city_id: defaultAddress?.city.city_id,
       },
       label: defaultAddress?.label,
+      district: defaultAddress?.district ?? "NO_DATA",
+      village: defaultAddress?.village,
     },
     mode: "onChange",
   });
@@ -59,6 +71,21 @@ export default function AddressForm({
   const { toast } = useToast();
   const { mutate } = useSWRConfig();
   const router = useRouter();
+
+  const districtFetcher = async () => {
+    const res = await fetch(
+      process.env.NEXT_PUBLIC_API_LOCATIONS_GET_DISTRICTS!,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          city_id: form.watch("city.city_id"),
+        }),
+      }
+    );
+
+    const response = await res.json();
+    return response.result as TDistrict[];
+  };
 
   const { data: provinces, isLoading: provincesLoading } = useSWR(
     "/api/shipping/get-provinces",
@@ -73,11 +100,21 @@ export default function AddressForm({
 
   const onProvinceChangeHandler = (province_id: string) => {
     form.setValue("city.province_id", province_id);
+    form.setValue("city.city_id", "");
+    form.setValue("district", "");
     mutate("/api/shipping/get-cities/" + province_id);
+    mutate("/api/shipping/get-districts", districtFetcher, {
+      populateCache: true,
+      revalidate: true,
+    });
   };
 
   const onCityChangeHandler = (city_id: string) => {
     form.setValue("city.city_id", city_id);
+    mutate("/api/shipping/get-districts", districtFetcher, {
+      revalidate: true,
+      rollbackOnError: false,
+    });
   };
 
   const { data: cities, isLoading: citiesLoading } = useSWR(
@@ -90,6 +127,23 @@ export default function AddressForm({
         .then((res) => res.json())
         .then((res) => res.data as TCity[])
   );
+
+  const { data: districtsData, isLoading: districtLoading } = useSWR(
+    "/api/shipping/get-districts",
+    districtFetcher
+  );
+
+  const onDistrictChangeHandler = (district: string) => {
+    form.setValue("district", district);
+  };
+
+  const onHelpModalOpen = (option: "district" | "village") => {
+    setHelp({ open: true, option: option });
+  };
+
+  const onHelpModalClose = () => {
+    setHelp({ open: false, option: "district" });
+  };
 
   const onSubmit: SubmitHandler<TAddressInput> = async (data) => {
     setIsLoading(true);
@@ -106,6 +160,8 @@ export default function AddressForm({
             recipient_name: data.recipient_name,
             recipient_phone: data.recipient_phone,
             label: data.label,
+            district: data.district ? data.district : "NO_DATA",
+            village: data.village ? data.village : "NO_DATA",
           }),
         }
       );
@@ -190,130 +246,192 @@ export default function AddressForm({
   };
 
   return (
-    <form
-      className="w-full rounded-lg overflow-hidden flex flex-col gap-4 lg:gap-8 p-4 lg:p-8"
-      onSubmit={
-        defaultAddress
-          ? form.handleSubmit(onUpdate)
-          : form.handleSubmit(onSubmit)
-      }
-    >
-      <div className="w-full flex flex-col gap-2">
-        <Label htmlFor="recipient_name">Nama Penerima</Label>
-        <Input
-          type="text"
-          id="recipient_name"
-          placeholder="Nama Penerima"
-          {...form.register("recipient_name")}
-        />
-      </div>
+    <>
+      <form
+        className="w-full rounded-lg overflow-hidden flex flex-col gap-4 lg:gap-8 p-4 lg:p-8"
+        onSubmit={
+          defaultAddress
+            ? form.handleSubmit(onUpdate)
+            : form.handleSubmit(onSubmit)
+        }
+      >
+        <div className="w-full flex flex-col gap-2">
+          <Label htmlFor="recipient_name">Nama Penerima</Label>
+          <Input
+            type="text"
+            id="recipient_name"
+            placeholder="Nama Penerima"
+            {...form.register("recipient_name")}
+          />
+        </div>
 
-      <div className="w-full flex flex-col gap-2">
-        <Label htmlFor="recipient_phone">Nomor Telepon Penerima</Label>
-        <Input
-          type="text"
-          inputMode="tel"
-          pattern="[0-9]*"
-          id="recipient_phone"
-          placeholder="08xxx"
-          {...form.register("recipient_phone")}
-        />
-      </div>
+        <div className="w-full flex flex-col gap-2">
+          <Label htmlFor="recipient_phone">Nomor Telepon Penerima</Label>
+          <Input
+            type="text"
+            inputMode="tel"
+            pattern="[0-9]*"
+            id="recipient_phone"
+            placeholder="08xxx"
+            {...form.register("recipient_phone")}
+          />
+        </div>
 
-      <div className="w-full flex flex-col gap-2">
-        <Label htmlFor="province">Provinsi</Label>
-        <Select
-          onValueChange={(value) => onProvinceChangeHandler(value)}
-          defaultValue={form.watch("city.province_id")}
-        >
-          <SelectTrigger className="w-full">
-            <SelectValue placeholder="Pilih provinsi" />
-          </SelectTrigger>
+        <div className="w-full flex flex-col gap-2">
+          <Label htmlFor="province">Provinsi</Label>
+          <Select
+            onValueChange={(value) => onProvinceChangeHandler(value)}
+            defaultValue={form.watch("city.province_id")}
+          >
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder="Pilih provinsi" />
+            </SelectTrigger>
 
-          <SelectContent className="overflow-y-auto max-h-[45vh]">
-            {provincesLoading ? (
-              <SelectItem value="loading" disabled>
-                Memuat provinsi...
-              </SelectItem>
-            ) : provinces ? (
-              provinces.map((province) => (
-                <SelectItem
-                  key={province.province_id}
-                  value={province.province_id}
-                >
-                  {province.province}
+            <SelectContent className="overflow-y-auto max-h-[45vh]">
+              {provincesLoading ? (
+                <SelectItem value="loading" disabled>
+                  Memuat provinsi...
                 </SelectItem>
-              ))
-            ) : (
-              <SelectItem value="error" disabled>
-                Tidak ada provinsi
-              </SelectItem>
-            )}
-          </SelectContent>
-        </Select>
-      </div>
-
-      <div className="w-full flex flex-col gap-2">
-        <Label htmlFor="city">Kota/Kabupaten</Label>
-        <Select
-          onValueChange={(value) => onCityChangeHandler(value)}
-          disabled={!form.watch("city.province_id")}
-          defaultValue={form.watch("city.city_id")}
-        >
-          <SelectTrigger className="w-full">
-            <SelectValue placeholder="Pilih kota/kabupaten" />
-          </SelectTrigger>
-
-          <SelectContent className="overflow-y-auto max-h-[45vh]">
-            {citiesLoading ? (
-              <SelectItem value="loading" disabled>
-                Memuat kota/kabupaten...
-              </SelectItem>
-            ) : cities ? (
-              cities.map((city) => (
-                <SelectItem key={city.city_id} value={city.city_id}>
-                  {properizeWords(city.type)} {city.city_name}
+              ) : provinces ? (
+                provinces.map((province) => (
+                  <SelectItem
+                    key={province.province_id}
+                    value={province.province_id}
+                  >
+                    {province.province}
+                  </SelectItem>
+                ))
+              ) : (
+                <SelectItem value="error" disabled>
+                  Tidak ada provinsi
                 </SelectItem>
-              ))
-            ) : (
-              <SelectItem value="error" disabled>
-                Tidak ada kota/kabupaten
-              </SelectItem>
-            )}
-          </SelectContent>
-        </Select>
-      </div>
+              )}
+            </SelectContent>
+          </Select>
+        </div>
 
-      <div className="w-full flex flex-col gap-2">
-        <Label htmlFor="label">Label</Label>
-        <Input
-          type="text"
-          id="label"
-          placeholder="Rumah/Kantor"
-          {...form.register("label")}
-        />
-      </div>
+        <div className="w-full flex flex-col gap-2">
+          <Label htmlFor="city">Kota/Kabupaten</Label>
+          <Select
+            onValueChange={(value) => onCityChangeHandler(value)}
+            disabled={!form.watch("city.province_id")}
+            defaultValue={form.watch("city.city_id")}
+          >
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder="Pilih kota/kabupaten" />
+            </SelectTrigger>
 
-      <div className="w-full flex flex-col gap-2">
-        <Label htmlFor="fullAddress">Alamat Lengkap</Label>
-        <Textarea
-          id="fullAddress"
-          placeholder="Kecamatan, Kelurahan, RT/RW, Kode Pos, Nomor Rumah"
-          rows={5}
-          {...form.register("fullAddress")}
-        />
-      </div>
+            <SelectContent className="overflow-y-auto max-h-[45vh]">
+              {citiesLoading ? (
+                <SelectItem value="loading" disabled>
+                  Memuat kota/kabupaten...
+                </SelectItem>
+              ) : cities ? (
+                cities.map((city) => (
+                  <SelectItem key={city.city_id} value={city.city_id}>
+                    {properizeWords(city.type)} {city.city_name}
+                  </SelectItem>
+                ))
+              ) : (
+                <SelectItem value="error" disabled>
+                  Tidak ada kota/kabupaten
+                </SelectItem>
+              )}
+            </SelectContent>
+          </Select>
+        </div>
 
-      <Button variant="default" type="submit" disabled={isLoading}>
-        {isLoading ? (
-          <>
-            <Loader2Icon className="w-4 h-4 mr-2 animate-spin" />
-            <span>Menyimpan...</span>
-          </>
-        ) : (
-          "Simpan"
-        )}
-      </Button>
-    </form>
+        <div className="w-full flex flex-col gap-2">
+          <div className="flex flex-row items-center gap-2">
+            <Label htmlFor="city">Kecamatan</Label>
+            <Button
+              variant="ghost"
+              size="icon"
+              type="button"
+              onClick={() => onHelpModalOpen("district")}
+            >
+              <HelpCircleIcon className="w-4 h-4" />
+            </Button>
+          </div>
+          <Select
+            onValueChange={(value) => onDistrictChangeHandler(value)}
+            disabled={
+              !form.watch("city.city_id") ||
+              districtLoading ||
+              (districtsData && districtsData.length < 1)
+            }
+            defaultValue={form.watch("district")}
+          >
+            <SelectTrigger className="w-full">
+              <SelectValue
+                placeholder={
+                  districtsData && districtsData.length < 1
+                    ? "Tidak ada data kecamatan"
+                    : "Pilih Kecamatan"
+                }
+              />
+            </SelectTrigger>
+
+            <SelectContent className="overflow-y-auto max-h-[45vh]">
+              {districtLoading ? (
+                <SelectItem value="loading" disabled>
+                  Memuat kota/kabupaten...
+                </SelectItem>
+              ) : districtsData && districtsData.length > 0 ? (
+                districtsData.map((district) => (
+                  <SelectItem key={district.code} value={district.name}>
+                    {properizeWords(district.name)}
+                  </SelectItem>
+                ))
+              ) : (
+                districtsData &&
+                districtsData.length < 1 && (
+                  <SelectItem value="NO_DATA" disabled>
+                    Tidak ada data kecamatan
+                  </SelectItem>
+                )
+              )}
+            </SelectContent>
+          </Select>
+        </div>
+
+        <div className="w-full flex flex-col gap-2">
+          <Label htmlFor="label">Label</Label>
+          <Input
+            type="text"
+            id="label"
+            placeholder="Rumah/Kantor"
+            {...form.register("label")}
+          />
+        </div>
+
+        <div className="w-full flex flex-col gap-2">
+          <Label htmlFor="fullAddress">Alamat Lengkap</Label>
+          <Textarea
+            id="fullAddress"
+            placeholder="Kecamatan, Kelurahan, RT/RW, Kode Pos, Nomor Rumah"
+            rows={5}
+            {...form.register("fullAddress")}
+          />
+        </div>
+
+        <Button variant="default" type="submit" disabled={isLoading}>
+          {isLoading ? (
+            <>
+              <Loader2Icon className="w-4 h-4 mr-2 animate-spin" />
+              <span>Menyimpan...</span>
+            </>
+          ) : (
+            "Simpan"
+          )}
+        </Button>
+      </form>
+
+      <AddressHelpModal
+        open={help.open}
+        onClose={onHelpModalClose}
+        option={help.option}
+      />
+    </>
   );
 }
