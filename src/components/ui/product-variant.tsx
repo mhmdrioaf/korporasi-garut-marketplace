946168b552aa8f4dcf9b38a9b0d359e64e53454f
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-01-31 22:31:44 +0700

    refactor: improving variant item card readability

--- src/components/ui/product-variant.tsx
+++ src/components/ui/product-variant.tsx
@@ -5,25 +5,31 @@ import { useDirectPurchase } from "@/lib/hooks/context/useDirectPurchase";
 export default function ProductVariants() {
   const { variants, product } = useDirectPurchase();
 
+  const variantItemCardStyle = (itemID: string, selectedID: string) => {
+    const baseStyle =
+      "p-1 w-full rounded-sm text-sm border border-input cursor-pointer text-center select-none";
+    const selectedStyle = `${baseStyle} bg-primary text-white font-bold`;
+
+    if (itemID === selectedID) {
+      return selectedStyle;
+    } else {
+      return baseStyle;
+    }
+  };
+
   if (product.variant) {
     return (
       <div className="w-full flex flex-col gap-4">
-        <p className="text-sm font-bold">
-          Harap memilih variant, jika tidak memilih, akan kami kirim secara
-          acak.
-        </p>
-        <div className="w-full grid grid-cols-2 gap-2">
-          <p>{product.variant.variant_title}</p>
-          <div className="w-full grid grid-cols-3 gap-2">
+        <div className="w-full flex flex-col gap-2">
+          <p className="font-bold">Varian {product.variant.variant_title}</p>
+          <div className="w-full grid grid-cols-2 gap-2">
             {product.variant.variant_item.map((item) => (
               <div
                 key={item.variant_item_id}
-                className={`p-1 w-full rounded-sm border border-input cursor-pointer text-center select-none ${
-                  variants.variantValue?.variant_item_id ===
+                className={variantItemCardStyle(
+                  variants.variantValue?.variant_item_id ?? "",
                   item.variant_item_id
-                    ? "bg-green-950 text-white font-bold"
-                    : ""
-                }`}
+                )}
                 onClick={() => variants.handler.onVariantsChange(item)}
               >
                 <p>{item.variant_name}</p>
