b6cafeb703fcc861d32f86dd377f06dba9475782
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-02-02 21:52:05 +0700

    refactor: adding getUserCart method

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,7 @@
 import Xendit from "xendit-node";
+import Carts from "./prisma-classes/Carts";
+import { db } from "./db";
+import { TCustomerCart } from "./globals";
 
 export async function getInvoice(invoice_id: string) {
   const xendit = new Xendit({
@@ -15,4 +18,12 @@ export async function getInvoice(invoice_id: string) {
     console.error("An error occurred while fetching invoice: ", error);
     return null;
   }
+}
+
+export async function getUserCart(user_id: number) {
+  const carts = new Carts(db.customer_cart, db.customer_cart_item);
+
+  const cart = await carts.getCart(user_id);
+
+  return cart as TCustomerCart | null;
 }
