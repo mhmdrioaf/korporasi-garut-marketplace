a815e55157eaef30d074018088f2c14ade0b4406
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-02-24 20:30:30 +0700

    feat(referral): adding referral link generator page

--- src/components/ui/referral/referral-generator.tsx
+++ src/components/ui/referral/referral-generator.tsx
@@ -18,43 +18,62 @@ interface IReferralGeneratorProps {
 export default function ReferralGenerator({
   products,
 }: IReferralGeneratorProps) {
-  const { form, loading, submit } = useReferral();
+  const { form, state, handler, linkRef } = useReferral({ products });
 
   return (
-    <form className="w-full flex flex-col gap-4 text-sm" onSubmit={submit}>
-      <div className="w-full flex flex-col gap-2">
-        <label htmlFor="name">Nama</label>
-        <Input
-          type="text"
-          required
-          {...form.register("name")}
-          placeholder="Isi dengan nama anda"
-        />
-      </div>
+    <div className="w-full flex flex-col gap-4">
+      <form
+        className="w-full flex flex-col gap-4 text-sm"
+        onSubmit={handler.submit}
+      >
+        <div className="w-full flex flex-col gap-2">
+          <label htmlFor="name">Nama</label>
+          <Input
+            type="text"
+            required
+            {...form.register("name")}
+            placeholder="Isi dengan nama anda"
+          />
+        </div>
 
-      <div className="w-full flex flex-col gap-2">
-        <label htmlFor="product_id">Produk</label>
-        <Select
-          onValueChange={(value) => form.setValue("product_id", Number(value))}
-          required
-        >
-          <SelectTrigger>
-            <SelectValue placeholder="Silakan pilih salah satu produk yang akan dipasarkan" />
-          </SelectTrigger>
+        <div className="w-full flex flex-col gap-2">
+          <label htmlFor="product_id">Produk</label>
+          <Select
+            onValueChange={(value) =>
+              form.setValue("product_id", Number(value))
+            }
+            required
+          >
+            <SelectTrigger>
+              <SelectValue placeholder="Silakan pilih salah satu produk yang akan dipasarkan" />
+            </SelectTrigger>
 
-          <SelectContent>
-            {products.map((product) => (
-              <SelectItem key={product.id} value={product.id.toString()}>
-                {product.title}
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
-      </div>
+            <SelectContent>
+              {products.map((product) => (
+                <SelectItem key={product.id} value={product.id.toString()}>
+                  {product.title}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
 
-      <Button variant="default" disabled={loading} type="submit">
-        {loading ? "Membuat tautan..." : "Buat Tautan"}
-      </Button>
-    </form>
+        <Button variant="default" type="submit">
+          Buat Tautan
+        </Button>
+      </form>
+
+      {state.generatedLink && (
+        <div className="w-full flex flex-col gap-2">
+          <p className="font-bold">Link Referral</p>
+          <div className="w-full flex flex-row items-center gap-2">
+            <Input type="text" ref={linkRef} value={state.generatedLink} />
+            <Button variant="default" onClick={handler.copyToClipboard}>
+              Salin
+            </Button>
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
--- src/components/ui/referral/useReferral.ts
+++ src/components/ui/referral/useReferral.ts
@@ -2,32 +2,67 @@
 
 import React from "react";
 import { useForm } from "react-hook-form";
+import { useToast } from "../use-toast";
+import { ROUTES } from "@/lib/constants";
 
 type TReferralInput = {
   name: string;
   product_id: number;
 };
 
-export function useReferral() {
-  const [loading, setLoading] = React.useState<boolean>(false);
+export function useReferral({ products }: { products: TProduct[] }) {
+  const [isCopied, setIsCopied] = React.useState<boolean>(false);
+  const [generatedLink, setGeneratedLink] = React.useState<string | null>(null);
+
+  const textRef = React.useRef<HTMLInputElement>(null);
   const form = useForm<TReferralInput>({
     mode: "onChange",
   });
 
-  const submit = form.handleSubmit(async (data) => {
-    setLoading(true);
-    try {
-      // await createReferral(data);
-    } catch (e) {
-      console.error(e);
-    } finally {
-      setLoading(false);
+  const { toast } = useToast();
+
+  const submit = form.handleSubmit((data) => {
+    const referrerName = data.name;
+    const referredProduct = data.product_id;
+    const product = products.find((product) => product.id === referredProduct);
+
+    if (product) {
+      const origin = window.location.origin;
+      const productURL = ROUTES.PRODUCT.DETAIL(product.id.toString());
+      const encodedReferrerName = encodeURIComponent(referrerName);
+      const url = `${origin}${productURL}?ref=${encodedReferrerName}`;
+
+      setGeneratedLink(url);
+    } else {
+      toast({
+        description: "Produk tidak ditemukan",
+      });
     }
   });
 
+  const copyToClipboard = () => {
+    if (textRef.current) {
+      navigator.clipboard.writeText(textRef.current.value);
+      setIsCopied(true);
+      toast({
+        description: "Tautan telah disalin.",
+      });
+    }
+  };
+
   return {
-    loading: loading,
     form: form,
-    submit: submit,
+
+    handler: {
+      submit,
+      copyToClipboard,
+    },
+
+    state: {
+      isCopied,
+      generatedLink,
+    },
+
+    linkRef: textRef,
   };
 }
